9e740d1f94ff9754547b6cbfbffc814ad4350319
e444er <e444er@@gmail.com>
2021-11-17 00:36:24 +0600

    finish

--- app/src/main/java/com/droidli/dictionaryapi/feature_dictionary/data/remote/dto/WordInfoDto.kt
+++ app/src/main/java/com/droidli/dictionaryapi/feature_dictionary/data/remote/dto/WordInfoDto.kt
@@ -1,5 +1,6 @@
 package com.droidli.dictionaryapi.feature_dictionary.data.remote.dto
 
+import com.droidli.dictionaryapi.feature_dictionary.data.local.entity.WordInfoEntity
 import com.droidli.dictionaryapi.feature_dictionary.domain.model.WordInfo
 
 data class WordInfoDto(
@@ -9,8 +10,8 @@ data class WordInfoDto(
     val phonetics: List<PhoneticDto>,
     val word: String
 ) {
-    fun toWordInfo(): WordInfo {
-        return WordInfo(
+    fun toWordInfoEntity(): WordInfoEntity {
+        return WordInfoEntity(
             meanings = meanings.map { it.toMeaning() },
             origin = origin,
             phonetic = phonetic,
--- app/src/main/java/com/droidli/dictionaryapi/feature_dictionary/domain/repository/WordInfoRepository.kt
+++ app/src/main/java/com/droidli/dictionaryapi/feature_dictionary/domain/repository/WordInfoRepository.kt
@@ -1,5 +1,10 @@
 package com.droidli.dictionaryapi.feature_dictionary.domain.repository
 
+import com.droidli.dictionaryapi.core.util.Resource
+import com.droidli.dictionaryapi.feature_dictionary.domain.model.WordInfo
+import kotlinx.coroutines.flow.Flow
+
 interface WordInfoRepository {
 
+    fun getWordInfo(word: String): Flow<Resource<List<WordInfo>>>
 }
